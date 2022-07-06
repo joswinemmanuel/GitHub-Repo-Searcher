b410366e31829d8a527c4e195aa91693969318a7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-06 07:47:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+from repos.api import repos_with_most_stars
+from repos.exceptions import GitHubApiException
+
+app = Flask(__name__)
+
+available_languages = ["Python", "JavaScript", "Ruby", "Java"]
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        # Use the list of all languages
+        selected_languages = available_languages
+    elif request.method == 'POST':
+        # Use the languages we selected in the request form
+        selected_languages = request.form.getlist("languages")
+
+    results = repos_with_most_stars(selected_languages)
+
+    return render_template(
+        'index.html',
+        selected_languages=selected_languages,
+        available_languages=available_languages,
+        results=results)
+
+
+@app.errorhandler(GitHubApiException)
+def handle_api_error(error):
+    return render_template('error.html', message=error)
+
