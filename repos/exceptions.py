1280c1c53a05477fe55b33d33e0873d6046407b9
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-07 07:24:42 +0530

    Add files via upload

---
+++ repos/exceptions.py
@@ -0,0 +1,9 @@
+class GitHubApiException(Exception):
+
+    def __init__(self, status_code):
+        if status_code == 403:
+            message = "Rate limit reached. Please wait a minute and try again."
+        else:
+            message = f"HTTP Status Code was: {status_code}."
+
+        super().__init__("A GitHub API Error Occurred: " + message)
